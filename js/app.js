8c45ba9253cc44871733bd24b24777503d190ced
Alyson Burton <funkyfrog3000@AlysonBrtonsMBP.attlocal.net>
2016-12-01 00:30:34 -0800

    addition of site

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.left_arrow').click(function() {
+      console.log('left clicked');
+    });
+    $('.right_arrow').click(function() {
+      console.log('right clicked');
+    });
+});
